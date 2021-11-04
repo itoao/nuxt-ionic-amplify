19435c0670ecda2b253caa6b6436c3464464f1ad
aoito <ao.ito@tabian.co>
2021-11-04 17:48:40 +0900

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+
 - まず、自動生成された type （214 行目の CorporateInfo など）を配列に全部ぶちこむ
+  - このとき、定義元の type と判定する式は、はじめのキーに __typename かどうか
+- type の内部で生成される __typename（GetProjectQuery など） が配列に格納された type  と合致していたら、置換する
+- 
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,39 @@
-// import ts, { factory } from 'typescript'
+import ts, { factory } from 'typescript'
 
-// 配列生成
-console.log("test")
+// ソースコードから AST に変換
+// AST からソースコードに変換
+export function generateCode <T extends ts.Node> (
+  nodes: ts.NodeArray<T>,
+  fileName: string = 'output.ts'
+) {
+  const resultFile = ts.createSourceFile(
+    fileName,
+    '',
+    ts.ScriptTarget.ESNext,
+    false,
+    ts.ScriptKind.TS
+  )
 
-// 
-// export function generateCode <T extends ts.Node> (
-//   nodes: ts.NodeArray<T>,
-//   fileName: string = 'output.ts'
-// ) {
-//   const resultFile = ts.createSourceFile(
-//     fileName,
-//     '',
-//     ts.ScriptTarget.ESNext,
-//     false,
-//     ts.ScriptKind.TS
-//   )
+  // AST をソースコードに復元
+  const printer = ts.createPrinter({
+    newLine: ts.NewLineKind.LineFeed // 改行コード
+  })
 
-//   const printer = ts.createPrinter({
-//     newLine: ts.NewLineKind.LineFeed
-//   })
+  // 定数 printer のルールで、リストを出力
+  return printer.printList(
+    ts.ListFormat.MultiLine,
+    nodes,
+    resultFile
+  )
+}
 
-//   return printer.printList(
-//     ts.ListFormat.MultiLine,
-//     nodes,
-//     resultFile
-//   )
-// }
+export function typeReferencer () {
+  const arr = []
+  
+}
+
+// AST から AST に変換（ここで、コンパクト化のロジックを書く）
+  // 配列作成
+    // 第一キーに __typename があれば、 作った配列に入れる（このとき、AST を触っている）
+      // if(SourceFile.statements.TypeAliasDeclaration.type.members.name.escapedText == "__typename")
+  // 
