e7ba47083e447a38451fdcd8f9efcb9a549f74ba
shiningbless <daiki.suzuki@tabian.co>
2021-11-08 13:13:06 +0900

    ビルドファイルの出力設定を修正

--- bin/index.ts
+++ bin/index.ts
@@ -9,84 +9,52 @@ export function transform() {
   const files = config.entry
   const program = ts.createProgram(files, {})
 
+  // 読み込みファイルが0だったら終了
   if (files.length === 0) { return false }
 
-  files.forEach(file => {
+  // まず出力ファイルを作成
+  const outputPath = config.output.dirName + '/' + config.output.fileName
 
+  // 出力ディレクトリが存在しなかったらディレクトリを作る
+  if(!fs.existsSync(config.output.dirName)){
+    fs.mkdirSync(config.output.dirName)
+  }
+
+  // 出力ファイルを初期化
+  fs.writeFileSync(outputPath, '')
+
+  // 読み込みファイル全てで処理を行う
+  files.forEach(file => {
+    // 読み込んだファイルからASTを作成
     const source = program.getSourceFile(file)
+
+    // ASTが存在したら
     if (source) {
+      // ASTをtransformer関数で変換
       const result = ts.transform(source, [transformer])
 
+      // プリンタを作成
       const printer = ts.createPrinter()
 
-      const printFile = ts.createSourceFile('test.ts', '', ts.ScriptTarget.Latest)
-      const code = printer.printNode(ts.EmitHint.Unspecified, result.transformed[0], printFile)
-      fs.writeFile(__dirname + '/dist.ts', code, err => { throw err })
+      // コードを出力し、ts.SourceFileとして出力
+      const printFile = ts.createSourceFile('', '', ts.ScriptTarget.Latest)
+
+      // 上記のts.SourceFile
+      const code: string = printer.printNode(ts.EmitHint.Unspecified, result.transformed[0], printFile)
+      fs.appendFileSync(outputPath, code)
     }
   })
 }
+
+
 export function transformer<T extends ts.Node>(context: ts.TransformationContext) {
   return (rootNode: T) => {
-    function visit(node: ts.Node): ts.Node {
-      if (ts.isUnionTypeNode(node!)) {
-        return createTypeReferenceUnionNode(node)
-      }
-      if (ts.isTypeLiteralNode(node)) {
-        return createTypeReferenceNode(node)
-      }
-        // if (ts.isTypeAliasDeclaration(node)) {
-        //   if (ts.isTypeLiteralNode(node.type)) {
-        //     ts.forEachChild(node.type, node_child => {
-        //       if (ts.isPropertySignature(node_child)) {
-        //         if (ts.isUnionTypeNode(node_child.type!)) {
-        //           return createTypeReferenceUnionNode(node_child.type)
-        //         }
-        //         if (ts.isTypeLiteralNode(node_child.type!)) {
-        //           return createTypeReferenceNode(node_child.type)
-        //         }
-        //       }
-        //     })
-        //   }
-        // }
+    function visit(node: ts.Node): ts.Node | null {
+
+      // ここに処理内容を書いていく
 
       return ts.visitEachChild(node, visit, context);
     }
     return ts.visitNode(rootNode, visit);
   }
 };
-
-
-/**
- * TypeLiteralNodeをTypeReferenceNodeに変換する関数
- * できなかった場合は既存のTypeLiteralNodeを返却する
- */
-export function createTypeReferenceNode(typeNode: ts.TypeLiteralNode): ts.TypeReferenceNode | ts.TypeLiteralNode {
-  typeNode.members.forEach(member => {
-    if (ts.isPropertySignature(member)) {
-      if (ts.isIdentifier(member.name)) {
-        if (member.name.escapedText === "___typename") {
-          if (ts.isLiteralTypeNode(member.type!)) {
-            if (ts.isStringLiteral(member.type.literal)) {
-              return factory.createTypeReferenceNode(member.type.literal.text)
-            }
-          }
-        }
-      }
-    }
-  })
-  return typeNode
-}
-
-export function createTypeReferenceUnionNode(typeNode: ts.UnionTypeNode) {
-  let typeNodes: ts.TypeNode[] = []
-  typeNode.types.forEach(type => {
-    if (ts.isTypeLiteralNode(type)) {
-      const typeReferenceNode = createTypeReferenceNode(type)
-      typeNodes.push(typeReferenceNode)
-    } else {
-      typeNodes.push(type)
-    }
-  })
-
-  return factory.createUnionTypeNode(typeNodes)
-}
